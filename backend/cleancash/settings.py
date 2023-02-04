9d82ce72c152cd6d04ba8f99617839ba46494a55
retarfbuildfax <lukasz.dlugajczyk@stxnext.pl>
2023-02-04 13:06:42 +0100

    [B] environment variables updated.

--- backend/cleancash/settings.py
+++ backend/cleancash/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-qw#*x8uf#mdihv9&7j-65gd09*i%4jmqx0b_qa=7f0-7@_nqj-'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DEBUG", True)
 
 ALLOWED_HOSTS = []
 
