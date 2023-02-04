8fb5c8d55866a6469911c3be6b70c5f9e314ef5d
retarfbuildfax <lukasz.dlugajczyk@stxnext.pl>
2023-02-04 12:38:44 +0100

    Base diagram of the project added.

--- frontend/src/components/dashboard/Orders.js
+++ frontend/src/components/dashboard/Orders.js
@@ -55,7 +55,7 @@ function preventDefault(event) {
 export default function Orders() {
   return (
     <React.Fragment>
-      <Title>Recent Orders</Title>
+      <Title>Recent Clean Ups</Title>
       <Table size="small">
         <TableHead>
           <TableRow>
