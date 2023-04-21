814b95153d78e1cb6a84e432861148bd01d25938
Łukasz Długajczyk <lukasz.dlugajczyk@stxnext.pl>
2023-04-21 12:00:32 +0200

    [backend] child field removed from salary list.

--- backend/salary/models.py
+++ backend/salary/models.py
@@ -1,8 +1,6 @@
 from django.db import models
-from children.models import Children
 
 
 class Salary(models.Model):
     date = models.DateField()
     value = models.DecimalField(max_digits=6, decimal_places=2)
-    child = models.ForeignKey(Children, on_delete=models.CASCADE)
