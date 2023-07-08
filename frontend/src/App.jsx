60c7e3a9bb5382959c5e4159a4d484bdfbe40d5e
Łukasz Długajczyk <lukasz.dlugajczyk@stxnext.pl>
2023-07-08 21:49:15 +0200

    [frontend] query.js core -> shared directory.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,7 @@
 import React from "react";
-import Button from "@mui/material/Button";
 import "@fontsource/roboto/400.css";
 import { BrowserRouter } from "react-router-dom";
 
-import { Dashboard } from "./features";
 import { MainContainer } from "./core";
 
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
--- frontend/src/core/api/index.jsx
+++ frontend/src/core/api/index.jsx
@@ -1,2 +1,2 @@
 export { default as Request } from "./client";
-export { default as Query } from "./query";
+export { default as Query } from "../../shared/query";
