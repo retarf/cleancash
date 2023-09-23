206c4a740e6abd259e60a9f13f67fb05780afa47
Łukasz Długajczyk <lukasz.dlugajczyk@stxnext.pl>
2023-09-23 12:27:39 +0200

    [F] Spies added to the test handlers.

---
+++ frontend/src/features/testUtils/spies.tsx
@@ -0,0 +1,25 @@
+import "@testing-library/jest-dom";
+
+export type spiesObject = {
+    get: jest.Mock<string>,
+    retrieve: jest.Mock<string>,
+    post: jest.Mock<string>,
+    patch: jest.Mock<string>,
+    delete: jest.Mock<string>,
+    resetAll: () => void
+}
+
+export const spies: spiesObject = {
+    get: jest.fn(() => "get"),
+    retrieve: jest.fn(() => "retrieve"),
+    post: jest.fn(() => "post"),
+    patch: jest.fn(() => "patch"),
+    delete: jest.fn(() => "delete"),
+    resetAll(): void {
+        this.get.mockReset();
+        this.retrieve.mockReset();
+        this.post.mockReset();
+        this.patch.mockReset();
+        this.delete.mockReset();
+    }
+}
